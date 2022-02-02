a03fd6b46310d4d1f1c3efc11904cf2d945a946f
Nick-Eagles <nick.eagles@libd.org>
2022-02-02 11:16:27 -0500

    Remake the SPEAQeasy submission script

--- code/01_SPEAQeasy/02_run_pipeline.sh
+++ code/01_SPEAQeasy/02_run_pipeline.sh
@@ -3,25 +3,24 @@
 #$ -o ../../processed-data/01_SPEAQeasy/SPEAQeasy_output.log
 #$ -e ../../processed-data/01_SPEAQeasy/SPEAQeasy_output.log
 #$ -cwd
-
-ORIG_DIR=/dcl01/lieber/ajaffe/Nick/SPEAQeasy
+#$ -N run_pipeline_02
 
 #  Get absolute path to 'joelPTSD_year2' repo
 base_dir=$(git rev-parse --show-toplevel)
 
-module load nextflow
+module load nextflow/20.01.0
 export _JAVA_OPTIONS="-Xms8g -Xmx10g"
 
-nextflow $ORIG_DIR/main.nf \
+nextflow $base_dir/code/SPEAQeasy/main.nf \
     --sample "paired" \
     --reference "hg38" \
     --strand "reverse" \
-    --annotation "/dcl01/lieber/ajaffe/Nick/SPEAQeasy/Annotation" \
-    -with-report "${base_dir}/processed-data/01_SPEAQeasy/execution_reports/JHPCE_run.html" \
+    --annotation "/dcs04/lieber/lcolladotor/annotationFiles_LIBD001/SPEAQeasy/Annotation" \
+    -with-report "${base_dir}/processed-data/01_SPEAQeasy/execution_reports/02_run_pipeline.html" \
     -w "${base_dir}/processed-data/01_SPEAQeasy/work" \
     --input "${base_dir}/processed-data/01_SPEAQeasy" \
     --output "${base_dir}/processed-data/01_SPEAQeasy/pipeline_output" \
-    -profile ptsd_y2
+    -profile jhpce
 
 #  Produces a report for each sample tracing the pipeline steps
 #  performed (can be helpful for debugging).
@@ -30,4 +29,4 @@ nextflow $ORIG_DIR/main.nf \
 #  section, and so if you rename the log, you must also pass replace the filename
 #  in the bash call below.
 echo "Generating per-sample logs for debugging..."
-bash $ORIG_DIR/scripts/generate_logs.sh ../../processed-data/01_SPEAQeasy/SPEAQeasy_output.log
+bash $base_dir/code/SPEAQeasy/scripts/generate_logs.sh $base_dir/processed-data/01_SPEAQeasy/SPEAQeasy_output.log
