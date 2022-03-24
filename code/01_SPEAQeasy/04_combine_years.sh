00c0160509c8c00c6652646682613ff80c2322c4
Nick-Eagles <nick.eagles@libd.org>
2022-03-24 14:47:25 -0400

    Require more memory

--- code/01_SPEAQeasy/04_combine_years.sh
+++ code/01_SPEAQeasy/04_combine_years.sh
@@ -1,7 +1,7 @@
 #$ -cwd
 #$ -o ../../processed-data/01_SPEAQeasy/04_combine_years.log
 #$ -e ../../processed-data/01_SPEAQeasy/04_combine_years.log
-#$ -l bluejay,mf=10G,h_vmem=10G,h_fsize=50G
+#$ -l bluejay,mf=70G,h_vmem=70G,h_fsize=50G
 #$ -N combine_years_04
 
 echo "**** Job starts ****"
