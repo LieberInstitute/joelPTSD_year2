3632da466bd09cd80d3f906096e8d53edc08885b
Nick-Eagles <nick.eagles@libd.org>
2022-05-20 14:39:06 -0400

    Output more info from the shell script

--- code/03_SPEAQeasy_Y2/04-combine_years.sh
+++ code/03_SPEAQeasy_Y2/04-combine_years.sh
@@ -5,5 +5,16 @@
 #$ -l mf=50G,h_vmem=50G
 #$ -N combine_years
 
+echo "**** Job starts ****"
+date
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+
 module load conda_R/4.1.x
 Rscript 04-combine_years.R
+
+echo "**** Job ends ****"
+date
